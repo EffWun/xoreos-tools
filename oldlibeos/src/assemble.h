fa6bb1008efe2700c9a07e2f0228459b572eac64
Sven Hesse <drmccoy@users.sourceforge.net>
2012-07-22 16:30:22 +0200

    OLDLIB: Fix GPL source headers

--- oldlibeos/src/assemble.h
+++ oldlibeos/src/assemble.h
@@ -1,8 +1,19 @@
 /* eos 0.0.1
  * Copyright (c) 2005 Sven Hesse (DrMcCoy)
  *
- * This file is part of eos and is distributed under the terms of
- * the GNU General Public Licence. See COPYING for more informations.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
 #ifndef _ASSEMBLE_H_
